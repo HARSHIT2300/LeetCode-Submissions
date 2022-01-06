54b731ed13767078f89de3d42bbf1ff6533a7217
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-01-06 20:03:21 +0530

    Time: 56 ms (30.91%), Space: 8.1 MB (8.36%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    bool check(unordered_map<char,int> &mp,unordered_map<char,int> &mp1)
-    {
-        for(auto &el : mp1)
-        {
-            if(mp[el.first]<el.second) return false;
-        }
-        return true;
-    }
+  
     int balancedString(string s) {
         string s1 = "";
         int n=s.size();
@@ -19,26 +12,34 @@ public:
             else if(el == 'W')w++;
             else e++;
         }
-        cout<<q<<" "<<w<<" "<<e<<" "<<r<<" ";
+      //  cout<<q<<" "<<w<<" "<<e<<" "<<r<<" ";
         while(q-(n/4)>0) {s1+="Q"; q--;}
         while(w-(n/4)>0) {s1+="W"; w--;}
         while(e-(n/4)>0) {s1+="E"; e--;}
         while(r-(n/4)>0) {s1+="R"; r--;}
         if(s1.size()==0) return 0;
-      //  cout<<s1<<" ";
-        unordered_map<char,int> mp,mp1;
+    //  cout<<s1<<" ";
+        unordered_map<char,int> mp1;
         for(auto &el : s1) mp1[el]++;
         int i=0,j=0,ans=s.size();
+        int ct = mp1.size();
         while(j<n)
         {
-            mp[s[j]]++;
-            while(check(mp,mp1))
-            {
+           if(mp1.find(s[j]) == mp1.end()){ j++; continue;}
+            
+            mp1[s[j]]--;
+            
+            if(!mp1[s[j]]) ct--;
+         //    cout<<i<<" "<<j<<" "<<ct<<"\n";
+            while(ct==0)
+            {   // cout<<i<<" "<<j<<" "<<ct<<"\n";
                 ans = min(ans,j-i+1);
-                mp[s[i]]--;
+                if(mp1.find(s[i]) != mp1.end()){
+                mp1[s[i]]++;
+                if(mp1[s[i]]>0) ct++;}
                 i++;
             }
-       //     cout<<i<<" "<<j<<" "<<ans<<"\n";
+          
             ++j;
         }
         return ans;
