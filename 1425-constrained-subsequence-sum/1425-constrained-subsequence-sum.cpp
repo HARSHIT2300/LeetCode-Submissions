b049700329290deb863f5b4ae5662506bcc129d1
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-01-09 13:18:37 +0530

    Time: 467 ms (25.86%), Space: 129.7 MB (38.62%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        vector<int> dp(1,nums[0]);
+        deque<int> dq; dq.push_back(nums[0]);
+        int n = nums.size();
+        int ans = nums[0];
+        for(int i=1;i<n;i++)
+        {
+            if(i>k && dq.front() == dp[i-k-1]) dq.pop_front();
+            dp.push_back(max(nums[i],dq[0]+nums[i]));
+            ans = max(dp[i],ans);
+            while(dq.size() && dq.back() < dp[i])dq.pop_back();
+            dq.push_back(dp[i]);
+        }
+        return ans;
+    }
+};
