31329cf30d72bb9367a21508081e14f70fff09b5
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-01-19 16:51:34 +0530

    Time: 100 ms (51.31%), Space: 6.1 MB (33.77%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int> pq;
+        pq.push(a); pq.push(b); pq.push(c);
+        int ans=0;
+        while(pq.size()>1)
+        {
+            int a=pq.top(); pq.pop(); 
+            int b = pq.top(); pq.pop();
+            a--; b--;
+            if(a) pq.push(a); 
+            if(b) pq.push(b);
+            ans++;
+        }
+        return ans;
+    }
+};
