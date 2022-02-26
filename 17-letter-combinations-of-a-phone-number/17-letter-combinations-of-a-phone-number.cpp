a66bfb4dd192ffa41ce8fd2def6c75950825e939
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-02-27 00:25:10 +0530

    Time: 2 ms (40.54%), Space: 6.4 MB (95.54%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void calc(string &digits,string res,vector<string> &v,vector<string> &ans)
-    {
-        if(digits.size() == res.size()){
-            if(res.size())
-            ans.push_back(res);
-            return ;}
-        int i=res.size();
-        int ch = digits[i]-50; 
-        calc(digits,res+v[ch][0],v,ans);
-        calc(digits,res+v[ch][1],v,ans);
-        calc(digits,res+v[ch][2],v,ans);
-        if(ch == 5 || ch == 7)
-            calc(digits,res+v[ch][3],v,ans);
-    }
     vector<string> letterCombinations(string digits) {
-        vector<string> v(8);
-        v[0]="abc";v[1]="def";v[2]="ghi";v[3]="jkl";v[4]="mno";v[5]="pqrs";v[6]="tuv";v[7]="wxyz";
-        
+        vector<string> v = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        stack<string> st;
+        st.push("");
         vector<string> ans;
-        calc(digits,"",v,ans);
+        while(!st.empty())
+        {
+            string res = st.top();
+              st.pop();
+            if(res.size() == digits.size()){if(res.size()) ans.push_back(res); continue;}
+            int ch = digits[res.size()]-50;
+            for(auto ch1 : v[ch])
+            {
+                st.push(res+ch1);
+            }
+          
+        }
         return ans;
     }
 };
