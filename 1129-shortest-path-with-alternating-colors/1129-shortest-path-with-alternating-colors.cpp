e9fafbe135026410944d44324acbeb34ae47837f
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-03-09 11:45:58 +0530

    Time: 11 ms (99.25%), Space: 14.8 MB (67.62%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -17,9 +17,12 @@ public:
             adj[1][v[0]].push_back(v[1]);
             // adj[1][v[1]].push_back(v[0]);
         }
+        //How to think/understand? ans[0][node] indicates the that the node 0 belongs to the red graph and and ans[1][node] indicates that the node 1 belongs to the blue bag
+    // also remember that if the current node value is of colour 0 all the corresponding updates of distance and pushing the child to the queue will be done with the child colour being 1 because the question has mentioned alternate colours
         queue<pair<int,int>> q;
         q.push({0,1});
         q.push({0,0});
+        // exploring each of the 2 possibilities that that 1st node,i.e, 0/1 can have edges starting from it to be red/blue respectively
      vector<vector<int>> dis(2,vector<int>(n,INT_MAX));
       dis[0][0]=0; dis[1][0]=0;
         vector<int> res(n,0);
@@ -36,6 +39,7 @@ public:
                 }
             }
           }
+        // we calculate the min of the dis of each nodes out of the 2 colours and assign it to res for that node's value. 
           for(int i=1;i<n;i++)
             {
                 int a = min(dis[0][i],dis[1][i]);
