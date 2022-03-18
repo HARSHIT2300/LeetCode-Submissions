ea1dc40e3c22bdd1818ee2787b18dabb8fb1a456
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-03-18 21:13:46 +0530

    Time: 52 ms (92.45%), Space: 22.2 MB (87.89%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,61 +1,28 @@
 class Solution {
 public:
-    vector<int> par,rank;
-    void unionn(int a,int b)
-    {
-        a = findPar(a); b=findPar(b);
-        if(a == b) return;
-        if(rank[a]<rank[b]) par[a]=b;
-        else if(rank[b]<rank[a]) par[b]=a;
-        else
-        {
-            par[a]=b; rank[b]++;
-        }
-    }
-    int findPar(int n)
-    {
-        if(par[n] == n) return n;
-        return par[n] = findPar(par[n]);
-    }
     int countServers(vector<vector<int>>& grid) {
-        int m = grid.size(),n = grid[0].size();
-        for(int i=0;i<m*n;i++)
-        {
-            par.push_back(i);
-            rank.push_back(0);
-        }
-        int ct_servers=0;
+        int m = grid.size(),n=grid[0].size();
+        int rows[m],cols[n];
+        memset(rows,0,sizeof(rows));
+        memset(cols,0,sizeof(cols));
         for(int i=0;i<m;i++)
-        {   int pidx=-1;
+        {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j])
-                {   cout<<"1"<<" ";
-                    ct_servers++;
-                    if(pidx != -1)
-                        unionn(pidx,n*i+j);
-                     pidx = n*i+j;
+                {
+                    rows[i]++; cols[j]++;
                 }
             }
         }
-         for(int i=0;i<n;i++)
-        {   int pidx=-1;
-            for(int j=0;j<m;j++)
-            {
-                if(grid[j][i])
-                {   //cout<<"1"<<" ";
-                   // ct_servers++;
-                    if(pidx != -1)
-                        unionn(pidx,n*j+i);
-                     pidx = n*j+i;
-                }
-            }
-        }
-     //   cout<<ct_servers<<" ";
-        for(int i=0;i<m*n;i++)
+        int ans=0;
+        for(int i=0;i<m;i++)
         {
-            if(grid[i/n][i%n]&& par[i] == i && !rank[i]) ct_servers--;
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]&&(rows[i]>1||cols[j]>1)) ans++;
+            }
         }
-        return ct_servers;
+        return ans;
     }
 };
