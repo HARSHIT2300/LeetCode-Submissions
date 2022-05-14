2e5bb6ceadb23dfbdd64971272a917c2367252e8
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-14 12:09:35 +0530

    Time: 182 ms (54.16%), Space: 95.7 MB (37.93%) - LeetHub

--- 676-implement-magic-dictionary/676-implement-magic-dictionary.cpp
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.cpp
@@ -1,33 +1,102 @@
-  map<string,int> mp;
+class Node{
+   
+    public :
+    Node* links[26];
+    bool flag;
+    Node()
+    {
+        for(int i=0;i<26;i++) links[i] = NULL;
+        flag = false;
+    }
+ };
+Node* root;
+ bool search_ag(string &Word,int i,int mv,Node* tmp)
+    {   
+        if(i == Word.size())
+        {   if(tmp->flag == false) return false;
+          
+            if(mv == 0) return true;
+            else return false;
+        }
+         int f=0;
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            if(tmp->links[Word[i]-97] == NULL) f++;
+        }
+        bool ans = false;
+        for(char ch = 'a';ch <= 'z';ch++)
+        {   int ch_val = ch-97;
+            if(tmp->links[ch_val] == NULL) continue;
+         
+            if(ch == Word[i]){
+                ans= search_ag(Word,i+1,mv,tmp->links[ch_val]);
+                if(ans) return ans;
+                if(f && mv)
+                  ans = search_ag(Word,i+1,0,tmp->links[ch-97]);
+                if(ans) return ans;
+            }
+            else
+            {
+                if(mv)
+                    ans = search_ag(Word,i+1,0,tmp->links[ch_val]);
+            }
+               if(ans) return ans; 
+        }
+        return ans;
+    }
 class MagicDictionary {
 public:
     
     MagicDictionary() {
-      mp.clear();
+     root = new Node();
+   MagicDictionary*   obj;
     }
     
     void buildDict(vector<string> dictionary) {
-        for(auto &el : dictionary)
+        for(auto &s : dictionary)
         {
-            mp[el]++;
-        }
-    }
-    
-    bool search(string searchWord) {
-        for(auto &w1 : mp)
-        {
-            int dif = 0,f=1;
-            if(searchWord.length() != w1.first.length()) continue;
-            for(int i=0;i<searchWord.length();i++)
-            {
-                if(searchWord[i] != w1.first[i]) dif++;
-                if(dif > 1){
-                    f=0; break;
+            Node *tmp = root;
+            for(auto &el : s)
+            {   int ch_val = el-97;
+                if(tmp->links[ch_val] != NULL)
+                    tmp = tmp->links[ch_val];
+                else
+                {
+                    Node* tmp1 = new Node();
+                    tmp->links[ch_val] = tmp1;
+                    tmp = tmp1;
                 }
             }
-            if(dif == 1) return true;
+            tmp->flag = true;
         }
-        return false;
+        
+    }
+   
+    bool search(string searchWord) {
+       
+        bool ans = false;
+        int f=0;
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            if(root->links[searchWord[0]-97] == NULL) f++;
+        }
+      for(char ch = 'a'; ch<='z'; ch++)
+      {
+          if(root->links[ch-97] == NULL) continue;
+          
+          if(ch == searchWord[0]){
+              ans= search_ag(searchWord,1,1,root->links[ch-97]);
+              if(ans) return ans;
+              if(f)
+                  ans = search_ag(searchWord,1,0,root->links[ch-97]);
+              if(ans) return ans;
+          }
+             
+          else
+           ans =  search_ag(searchWord,1,0,root->links[ch-97]);
+          if(ans) return ans;
+      }
+        return ans;
     }
 };
 
