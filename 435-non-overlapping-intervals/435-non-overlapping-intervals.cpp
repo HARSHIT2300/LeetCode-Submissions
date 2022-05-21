995683f600d9fa5dc3aa63ebc6cea2f17301b498
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-22 00:48:11 +0530

    Time: 740 ms (22.23%), Space: 89.6 MB (97.34%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,19 +1,22 @@
-bool cmp(vector<int>& v1,vector<int>& v2)
-{
-   
-    return v1[1] < v2[1];
-}
 class Solution {
 public:
+    static bool cmp(vector<int> &v1,vector<int> &v2)
+    {
+        return v1[1]<v2[1];
+    }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-       sort(intervals.begin(),intervals.end(),cmp);
-        int ed = intervals[0][1];
-        int ans = 0;
+        // need to find maximum no of non overlapping intervals and then subtract it from total no of intervals.  It will give us minimum intervals reqd to make the intervals non overlapping
+        sort(intervals.begin(),intervals.end(),cmp);
+        int end_pt=intervals[0][1];
+        int ct_non_overlap_inter=1;
         for(int i=1;i<intervals.size();i++)
         {
-            if(intervals[i][0] < ed) ans++;
-            else ed = intervals[i][1];
+            if(intervals[i][0]>=end_pt)
+            {
+                end_pt=intervals[i][1];
+                ct_non_overlap_inter+=1;
+            }
         }
-        return ans;
+        return intervals.size()-ct_non_overlap_inter;
     }
 };
