82a4272b61d547ad6e2c366005aded87a322c5bc
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-25 19:38:25 +0530

    Time: 27 ms (68.58%), Space: 12.7 MB (12.03%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -5,7 +5,10 @@ void handler(int n,map<int,int> &mp)
         mp[n] = 1; return ;
     }
     auto it = mp.lower_bound(n);
+
     if(it!=mp.end() && it->first == n) return ;
+    // if the value n  already exists in the lis, no point of taking it into consideration again as the lis will not change/get affected
+    
     if(it == mp.begin())
     {
         mp.erase(it);
@@ -14,12 +17,13 @@ void handler(int n,map<int,int> &mp)
     }
     it--; int adv = it->second+1;
     it = mp.upper_bound(n);
-    if(it!= mp.end() && it->first>n)
+    
+    if(it!= mp.end() && it->first>n) // if current element,n, is smaller than the upper bound of n , we remove the upper bound of n as  now with a smaller element ,i.e n we can have the same advantage and this seems like an more intuitive task that could possibily improve our chances of getting a larger lis while taking into consideration the subsequent unknown numbers that would coming in nums ....
         mp.erase(it);
     
     mp[n] = adv;
 }
-
+// lis is an increasing sequence that we are maintaing
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -29,15 +33,12 @@ public:
         for(auto &el : nums)
         {
             handler(el,lis);
-        //       for(auto &el : lis)
-        // {
-        //   cout<<el.first<<" "<<el.second<<"--";} cout<<"\n";
-        // }
+    
         }
         int ans = 1;
         for(auto &el : lis)
         {
-        //  cout<<el.first<<" "<<el.second<<"\n";
+      
             ans = max(ans,el.second);
         }
         return ans;
