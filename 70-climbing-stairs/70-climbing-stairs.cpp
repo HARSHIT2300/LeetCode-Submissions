1a02657f29bfa353ca16c11dc5fe56b059e09787
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-26 16:44:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.53%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        int ways[n+1];
+        memset(ways,-1,sizeof(ways));
+        ways[1] = 1; ways[2] = 2;
+        for(int i = 3;i<=n;i++)
+        {
+            ways[i] = ways[i-1]+ways[i-2];
+        }
+        return ways[n];
+    }
+};
