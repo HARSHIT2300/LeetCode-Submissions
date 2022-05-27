7f3ed998288eec4fc332d1e75916c097e269606c
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-27 20:59:57 +0530

    Time: 28 ms (80.98%), Space: 7.1 MB (81.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         // errichto's approach
+        //even length and odd length strings are possible and considering the initial position we then radiate  low and high in opposite directions... more intuitive + easy to think + doesn't use up extra space 
         int n = s.size();
-        int dp[n][n];
+       
        
         int res_size = 1;
         int start = 0;
-        for(int i=0;i<n;i++)
-        {
-            dp[i][i] = 1;
-            if(i<n-1 && s[i] == s[i+1])
-                dp[i][i+1] = 1;
-        }
+
         for(int pos = 0; pos<n; pos++)
         {
             int low = pos-1, high  = pos+1;
