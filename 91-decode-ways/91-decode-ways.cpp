5e3acaf5e0a65c0276e14a96980ad35ac389ce30
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-28 13:27:11 +0530

    Time: 6 ms (24.12%), Space: 6.2 MB (45.07%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,4 +1,4 @@
-int dfs(string s,int i,vector<int> &ans)
+/* int dfs(string s,int i,vector<int> &ans)
 {
     if(ans[i] !=-1) return ans[i];
     if(i == s.size())
@@ -11,12 +11,27 @@ int dfs(string s,int i,vector<int> &ans)
     return ans[i];
     
 }
+// Bottom Up Recursive
+*/
+
 // dp[i] will represnt/store the ans from index i(0-based) till the end of the string...
 class Solution {
 public:
+// Top down iterative
     int numDecodings(string s) {
       vector<int> ans(s.size()+1,-1);
-     int a = dfs(s,0,ans);
+    // int a = dfs(s,0,ans);
+        int n = s.size();
+        ans[n] = 1;
+        for(int i = n-1;i>=0;i--)
+        {
+            if(s[i] == '0')
+                ans[i] = 0;
+            else
+                ans[i] = ans[i+1];
+            if(i!=n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1]-'0' <= 6)))
+                ans[i]+=ans[i+2];
+        }
         return ans[0];
     }
 };
