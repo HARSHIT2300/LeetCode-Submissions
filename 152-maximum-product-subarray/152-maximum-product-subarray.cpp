05d80ac5209f4e471d91fa2b6f88a3fb72531c54
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-29 00:46:09 +0530

    Time: 16 ms (19.85%), Space: 13.8 MB (27.46%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int p1=1,p2=1,ct=0;
+        int ans = nums[0];
+        for(auto &el : nums)
+        {
+            if(el == 0)
+            {
+                ans = max(ans,0);
+                p1=1;p2=1; ct=0; continue;
+            }
+            if(ct%2){
+                p2*=el; ans=max(ans,p2);}
+            else{
+                p1*=el; ans=max(ans,p1);}
+            
+            if(el<0)
+            {
+                if(ct%2)
+                {
+                    p1*=p2; p2=1;
+                    ans = max(p1,ans);
+                }
+                // else
+                // {
+                //     p2=1;
+                // }
+                ct++;
+            }
+      //      cout<<ans<<" "<<p1<<" "<<p2<<"\n";
+        }
+        p1=1;p2=1;ct=0;
+        for(int i = nums.size()-1;i>=0;i--)
+        {   int el = nums[i];
+            if(el == 0)
+            {
+                ans = max(ans,0);
+                p1=1;p2=1; ct=0; continue;
+            }
+            if(ct%2){
+                p2*=el; ans=max(ans,p2);}
+            else{
+                p1*=el; ans=max(ans,p1);}
+            
+            if(el<0)
+            {
+                if(ct%2)
+                {
+                    p1*=p2; p2=1;
+                    ans = max(p1,ans);
+                }
+                // else
+                // {
+                //     p2=1;
+                // }
+                ct++;
+            }
+          //  cout<<ans<<" "<<p1<<" "<<p2<<"\n";
+        }
+        return ans;
+    }
+};
