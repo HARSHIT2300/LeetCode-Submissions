1adc10bcea036965031ff53f280e22457ef97a61
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-29 01:03:47 +0530

    Time: 11 ms (51.38%), Space: 13.9 MB (27.46%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        
+        // Computed the product considering prefixes firstly and then suffixes later...
+        // Used 2 loops respectively for each of the above tasks.. computed and then finally stored the ans..
+        // p1 stores the product when currently the negatives are even and when negatives are odd p2 stores the product
         int p1=1,p2=1,ct=0;
         int ans = nums[0];
         for(auto &el : nums)
