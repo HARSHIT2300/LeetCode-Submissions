1a5ec0812b6faa9590ef05d33a95f5a6064464c0
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-05-29 19:32:23 +0530

    Time: 19 ms (98.00%), Space: 16.6 MB (47.85%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,16 +1,25 @@
-// to check
+int calc(vector<int>&nums,int i,int sm,vector<int>& dp)
+{
+    if(sm == 0) return 1;
+   if(sm<0 || i == nums.size()) return 0;
+    if(dp[sm] !=-1) return dp[sm];
+    dp[sm] = calc(nums,i+1,sm-nums[i],dp) || calc(nums,i+1,sm,dp);
+    return dp[sm];
+   
+}
+
 class Solution {
 public:
-    int8_t dp[10001] = {[0 ... 10000] = -1};
     bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        if(totalSum & 1) return false;
-        return subsetSum(nums, totalSum / 2);
-    }
-    bool subsetSum(vector<int>& nums, int sum, int i = 0) {
-        if(sum == 0) return true;
-        if(i >= size(nums) || sum < 0) return false; 
-        if(dp[sum] != -1) return dp[sum];
-        return dp[sum] = subsetSum(nums, sum - nums[i], i + 1) || subsetSum(nums, sum, i + 1);
+        int target = 0;
+        for(auto &el : nums) target+=el;
+        if(target&1) return false;
+        target>>=1;
+        vector<int> dp(20004,-1);
+        dp[0] = 1;
+        calc(nums,0,target,dp);  
+      //  for(int i=1;i<100;i++) cout<<dp[i]<<" ";
+     //   cout<<dp[target]<<" "<<target<<" ";
+        return dp[target];
     }
 };
