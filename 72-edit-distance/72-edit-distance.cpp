e010eae4c8c39fac0221b0b89467996f1e2a8099
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-06-01 22:11:56 +0530

    Time: 23 ms (41.50%), Space: 9.2 MB (20.26%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,15 +1,20 @@
 #define INF 10000001
 int calc(int i1,int i2,int &s1,int &s2,string &w1,string &w2,vector<vector<int>> &dp)
-{
-   
+{   
     if(i2 == s2) return dp[i1][i2] = s1-i1;
+    // i2 has reached end of w2 so the extra characters left in w1 need to be deleted...
     if(i1 == s1) return dp[i1][i2] = s2-i2;
+    //i1 has reached end of i1 so so the extra characters in w2, need to be added in w1
      if(dp[i1][i2] != INF) return dp[i1][i2];
     if(w1[i1] == w2[i2])
         dp[i1][i2] = calc(i1+1,i2+1,s1,s2,w1,w2,dp);
     else{
         dp[i1][i2] = min(dp[i1][i2],min(calc(i1+1,i2+1,s1,s2,w1,w2,dp),calc(i1,i2+1,s1,s2,w1,w2,dp))+1);
+        // first case represent replace operation
+        // second case represrnts insert operation
+      
     dp[i1][i2] = min(dp[i1][i2],calc(i1+1,i2,s1,s2,w1,w2,dp)+1);
+          // third case represents delete operation
     }
     return dp[i1][i2];
 }
