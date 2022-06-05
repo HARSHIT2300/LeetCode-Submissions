ec723a45195647cd07a181c74f7e596621666be0
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-06-06 00:57:04 +0530

    Time: 8 ms (38.47%), Space: 7.4 MB (16.41%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int ans = 0,n=s.length();
+        int dp[s.size()+1];
+        memset(dp,0,sizeof(dp));
+        int n = s.size();
         stack<int> st;
-        for(int i=0;i<n;i++)
+        int ans = 0;
+        for(int i = 1;i<=n;i++)
         {
-            if(s[i] == '(') st.push(i);
+            if(s[i-1] == '(')
+            {
+                st.push(i);
+                if(i == 1 || s[i-2] == ')')
+                dp[i]  = dp[i-1];
+                else dp[i] =0;
+            }
             else
             {
-                if(!st.empty() && s[st.top()] == '(') st.pop();
+                if(st.empty())
+                    dp[i] = 0;
                 else
-                    st.push(i);
+                {
+                    int a = st.top(); st.pop();
+                    dp[i]  = dp[a]+i-a+1;
+                }
             }
+           // cout<<dp[i]<<" ";
+            ans = max(ans,dp[i]);//cout<<st.size()<<" ";
         }
-        if(st.empty()) return n;
-        int r  = n;
-        while(!st.empty())
-        {
-            ans = max(ans, r - st.top()-1);
-            r = st.top();
-            st.pop();
-        }
-        ans = max(ans,r);
         return ans;
     }
 };
