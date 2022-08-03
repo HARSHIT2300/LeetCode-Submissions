e7b18e1e1f9563b646f75fe7a22140c12f9de818
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-08-04 00:16:46 +0530

    Time: 506 ms (42.08%), Space: 129.3 MB (18.80%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -1,48 +1,39 @@
+/// JUST TO TC Of OTHER's SOLN
+
 class Solution {
 public:
-    long long maxPoints(vector<vector<int>>& points1) {
-        // calc max prefix  max suffix for each row and add max out of these to the next row
-        //0 - n-1 
+    long long maxPoints(vector<vector<int>>& points) {
+        vector<vector<long long>> dp(points.size(), vector<long long>(points[0].size(), -1));
         
-        long long int ans = 0LL;
-        int m = points1.size();
-        int n = points1[0].size(); // m rows n cols
-        vector<vector<long long int>> points;
-        for(int row=0; row < m; row++)
-        {   vector<long long int> v;
-            for(int col = 0; col< n ;col++)
-            {
-                v.push_back(points1[row][col]);
-            }
-         points.push_back(v);
+        for (int i = 0; i < points[0].size(); ++i) {
+            dp[0][i] = points[0][i];
         }
-        for(int row = 0; row < m-1 ; row++)
-        {   
-            vector<long long int> lmax(n,0); lmax[0] = (long long int)points[row][0];
-            vector<long long int> rmax(n,0); rmax[n-1] = (long long int)points[row][n-1];
+        
+        for (int i = 1; i < points.size(); ++i) {
+            vector<long long> left_dp(points[i].size(), -1);
+            vector<long long> right_dp(points[i].size(), -1);
             
-            for(int col = 1;col < n; col++)
-            {
-                lmax[col] = max((long long int)points[row][col],lmax[col-1]-1);
+            left_dp[0] = dp[i - 1][0];
+            for (int k = 1; k < points[i].size(); ++k) {
+                left_dp[k] = max(left_dp[k - 1], dp[i - 1][k] + k);
             }
-            for(int col = n-2;col >=0; col--)
-            {
-                rmax[col] = max((long long int)points[row][col],rmax[col+1]-1);
+            
+            right_dp.back() = dp[i - 1].back() - points[i].size() + 1;
+            for (int k = points[i].size() - 2; k >= 0; --k) {
+                right_dp[k] = max(right_dp[k + 1], dp[i - 1][k] - k);
             }
-            for(int col= 0; col < n;col++)
-            {
-                points[row+1][col]+=max(lmax[col],rmax[col]);
+            
+            for (int j = 0; j < points[i].size(); ++j) {
+                dp[i][j] = max(left_dp[j] - j, right_dp[j] + j) + points[i][j];
             }
-        //    cout<<points[row+1][0]<<" ";
-        }
-      //  cout<<m<<" "<<n<<"\n";
-        for(int col = 0;col < n; col++)
-        {  // cout<<1<<" ";
-            ans = max(ans,(long long int)points[m-1][col]);
-          //  cout<<ans<<"\n";
         }
         
-    return (long long)ans;
-    } 
+        long long max_ans = -1;
+        for (const auto v : dp.back()) {
+            max_ans = max(max_ans, v);
+        }
+        
+        return max_ans;
+    }
 
 };
