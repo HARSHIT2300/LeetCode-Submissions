3f948aef726a8c801ca2fde955820c1cbc610cab
Harshit Yadav <56017713+HARSHIT2300@users.noreply.github.com>
2022-08-22 21:23:22 +0530

    Time: 2794 ms (5.07%), Space: 444.5 MB (5.31%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        vector<int> v(10003,0);
+        int l_idx=0;
+        map<int,set<int>> mp;
+        v[0]=1; mp[nums[0]].insert(0);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(mp[nums[i]-1].size() == 0)
+            {
+                ++l_idx;
+                v[l_idx]+=1;
+                mp[nums[i]].insert(l_idx);
+            }
+            else
+            {
+                set<int> s = mp[nums[i]-1];
+                int id = *(s.begin());
+                int mn = v[id];
+                for(auto &el : s)
+                {
+                    if(v[el] <= mn)
+                    {
+                        mn = v[el];
+                        id = el;
+                    }
+                }
+                mp[nums[i]].insert(id);
+                v[id]+=1;
+                mp[nums[i]-1].erase(mp[nums[i]-1].find(id));
+            }
+        }
+        for(int i=0;i<=l_idx;i++)
+        {   auto el = v[i];
+            cout<<el<<" ";
+            if(el < 3) return false;
+        }
+        return true;
+    }
+};
